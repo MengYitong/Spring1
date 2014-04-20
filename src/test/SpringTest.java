1e18faa594162e46651b7f1b50d71321533ae639
MengYitong <mengyitongge@163.com>
2014-04-20 21:27:20 +0800

    此bean.xml的配置测试通过
    如果出现如下错误描述：
    cvc-elt.1：找不到元素“beans”的声明
    请参考本bean.xml文件

---
+++ src/test/SpringTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+	public static void main(String[] args)
+	{
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("bean.xml");
+		System.out.println(ctx);
+		PersonService p=ctx.getBean("personService",PersonService.class);
+		p.info();
+	}
+}
